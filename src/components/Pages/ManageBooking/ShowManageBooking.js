071f0131116927e5230c7c0eb6da5c741e66fbec
Mohib2021 <mdmohibullasheikh@gmail.com>
2021-10-31 20:10:37 +0600

    test everything

--- src/components/Pages/ManageBooking/ShowManageBooking.js
+++ src/components/Pages/ManageBooking/ShowManageBooking.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, ListGroup } from "react-bootstrap";
 
 function ShowManageBooking({ booking, deletePackage, approve }) {
 	// destructuring booking data
-	const { packageName, packageImg, status, _id } = booking;
+	const { packageName, packageImg, status, _id, email, address, fullName } =
+		booking;
 	const shadow = {
 		boxShadow: "0px 0px 10px gray",
 	};
 
 	return (
-		<Col md={3} sm={6} xs={12}>
-			<div data-aos="fade-right" className="my-3 my-md-4 p-2 " style={shadow}>
-				<img className="img-fluid" src={packageImg} alt="package-Img" />
-				<h4>{packageName}</h4>
+		<Col md={4} sm={6} xs={12}>
+			<div
+				data-aos="fade-right"
+				className="my-3 my-md-4 p-3 rounded "
+				style={shadow}
+			>
+				<img className="img-fluid rounded" src={packageImg} alt="package-Img" />
+				<h4 className="mt-2">{packageName}</h4>
 				<div className="blockquote-footer ms-5 my-3">
 					<b>{status}</b>
 				</div>
+				<ListGroup className="small mb-3">
+					<ListGroup.Item>Name : {fullName}</ListGroup.Item>
+					<ListGroup.Item>Mail : {email}</ListGroup.Item>
+					<ListGroup.Item>Address : {address}</ListGroup.Item>
+				</ListGroup>
+
 				<button onClick={() => approve(_id)} className="btn btn-dark me-3">
 					Approve
 				</button>
