d3ba2be4c269686b593412818f832ca1031399b1
Mohib2021 <mdmohibullasheikh@gmail.com>
2021-12-11 07:03:47 +0600

    add banner

--- src/components/Pages/Home/Carousal.js
+++ src/components/Pages/Home/Carousal.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-
+import style from "./Carousal.module.css";
 function Carousal() {
 	return (
 		<div>
 			<Carousel>
 				<Carousel.Item>
 					<img
-						className="d-block w-100"
+						className={`d-block ${style.banner} w-100`}
 						src="https://images.pexels.com/photos/41174/action-air-aircraft-aviation-41174.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 						alt="Third slide"
 					/>
@@ -16,7 +16,7 @@ function Carousal() {
 				</Carousel.Item>
 				<Carousel.Item>
 					<img
-						className="d-block w-100"
+						className={`d-block ${style.banner} w-100`}
 						src="https://images.unsplash.com/photo-1615646194267-ecf4380ac001?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
 						alt="First slide"
 					/>
@@ -25,7 +25,7 @@ function Carousal() {
 				</Carousel.Item>
 				<Carousel.Item>
 					<img
-						className="d-block w-100"
+						className={`d-block ${style.banner} w-100`}
 						src="https://images.pexels.com/photos/5387999/pexels-photo-5387999.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 						alt="Second slide"
 					/>
